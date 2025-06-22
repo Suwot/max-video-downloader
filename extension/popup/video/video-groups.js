af03d309316a9c661e1795894da7fb03b805f980
Rostislav <azebaku98@gmail.com>
2025-06-22 16:37:43 +0300

    upd: some styles fixed

--- extension/popup/video/video-groups.js
+++ extension/popup/video/video-groups.js
@@ -60,7 +60,7 @@ export function createTypeGroup(type, videos) {
     const toggle = document.createElement('div');
     toggle.className = 'media-type-toggle';
     toggle.innerHTML = `
-        <svg viewBox="0 0 24 24" width="16" height="16">
+        <svg viewBox="0 0 20 20" width="16" height="16">
             <path d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6-6-6 1.41-1.41z"/>
         </svg>
     `;
